5fea5c5d41a90990da9f92badd2fb7aa621469c2
juneglee <klcpop1@gmail.com>
2021-08-06 16:56:16 +0900

    add : RestAPI Post Request

--- annotation/pybo/serializers.py
+++ annotation/pybo/serializers.py
@@ -7,6 +7,10 @@ class RoomSerializer(serializers.ModelSerializer):
         fields = ('id','code','host','guest_can_pause'
                   ,'votes_to_skip','created_at')
 
+class CreateRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('guest_can_pause', 'votes_to_skip')
 
 # class ShapeSerializer(serializers.Serializer):
 #     type = serializers.ChoiceField(choices=models.ShapeType.choices())
